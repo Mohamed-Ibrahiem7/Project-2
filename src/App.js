63dcb5df0fe3f05d0cbad11c65d7ab1ef9c72422
Mohamed-Ibrahiem7 <mohamed.ibrahiem863@gmail.com>
2022-09-06 03:39:58 +0200

    feat & docs

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Home from './Components/pages/Home/index';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
